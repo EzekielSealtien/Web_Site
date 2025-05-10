de505a164f00454dcfe5aa94c50b10c4e9eca9ee
EzekielSealtien <EzekielTakam@teccart.online>
2025-05-10 02:32:59 -0400

    Update connexion.php

--- app/configs/connexion.php
+++ app/configs/connexion.php
@@ -1,14 +1,14 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-define('DB_SERVEUR', $_ENV['DB_SERVEUR']);
-define('DB_NOM', $_ENV['DB_NOM']);
+define('DB_SERVEUR', getenv('DB_SERVEUR'));
+define('DB_NOM', getenv('DB_NOM'));
 define('DB_DSN', 'pgsql:host=' . DB_SERVEUR . ';dbname=' . DB_NOM . ';port=5432');
-define('DB_LOGIN', $_ENV['DB_LOGIN']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_LOGIN', getenv('DB_LOGIN'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 global $oPDO;
 
